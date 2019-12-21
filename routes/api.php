075c0a7d243b17fe7950b68f4b4acff8d1d5d903
Pritesh Shinde <priteshs@prdxn.com>
2019-12-21 22:59:03 +0530

    Laravel routes modified

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,22 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware(['api.cors'])->group(function () {
+    Route::get('/unauthenticated', function () {
+        return response()->json(["error" => "Unauthenticated user!"], 401);
+    })->name('unauthenticated');
+
+    Route::get('/unauthorized', function () {
+        return response()->json(["error" => "Unauthorized user!"], 403);
+    })->name('unauthorized');
+
     Route::middleware('auth:api')->get('/user', function (Request $request) {
         return $request->user();
     });
     Route::prefix('users')->group(function () {
         Route::get('/', 'Api\UserController@getUsers')->name('get.users');
     });
+    // Default 404 route for APIs, make sure to always keep this at the end of file.
+    Route::any('/{any}', function () {
+        return response()->json(["error" => "Not found!"], 404);
+    })->where('any', '.*')->name('not.found');
 });
