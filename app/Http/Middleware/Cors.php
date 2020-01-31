263778868550db021268e3681b65830ea0e0fc25
Pritesh Shinde <priteshs@prdxn.com>
2020-01-31 13:08:53 +0530

    Cors middleware modified

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,10 +15,22 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', env('CLIENT_URL') || '*')
-            ->header('Access-Control-Allow-Headers', 'Origin, Authorization, X-Requested-With, Content-Type, Accept')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Accept', 'application/json');
+        $headers = [
+            'Access-Control-Allow-Origin'      => env('CLIENT_URL') || '*',
+            'Access-Control-Allow-Methods'     => 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Max-Age'           => '86400',
+            'Access-Control-Allow-Headers'     => 'Origin, Authorization, X-Requested-With, Content-Type, Accept',
+            'Accept'                           => 'application/json'
+        ];
+
+        if ($request->isMethod('OPTIONS'))
+            return response()->json(["method" => "OPTIONS"], 200, $headers);
+
+        $response = $next($request);
+        foreach($headers as $key => $value)
+            $response->header($key, $value);
+
+        return $response;
     }
 }
